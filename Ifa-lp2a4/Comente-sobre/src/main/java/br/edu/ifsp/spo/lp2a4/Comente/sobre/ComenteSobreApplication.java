d1dd25b7bc4dc510bf8fef082ad6846c152cb407
welenmota75 <71537729+welenmota75@users.noreply.github.com>
2023-06-21 02:53:51 -0300

    Add files via upload

---
+++ Ifa-lp2a4/Comente-sobre/src/main/java/br/edu/ifsp/spo/lp2a4/Comente/sobre/ComenteSobreApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.spo.lp2a4.Comente.sobre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComenteSobreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComenteSobreApplication.class, args);
+	}
+
+}
---
+++ Ifa-lp2a4/Comente-sobre/src/test/java/br/edu/ifsp/spo/lp2a4/Comente/sobre/ComenteSobreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.spo.lp2a4.Comente.sobre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComenteSobreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
