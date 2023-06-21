eadd2f40408180098fb5bad21b485b978b581bfc
welenmota75 <71537729+welenmota75@users.noreply.github.com>
2023-06-21 17:20:45 -0300

    Add files via upload

---
+++ Comente-Sobre/src/test/java/br/edu/ifsp/spo/lp2a4/Comente/Sobre/ComenteSobreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.spo.lp2a4.Comente.Sobre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComenteSobreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
